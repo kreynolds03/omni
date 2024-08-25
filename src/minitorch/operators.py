ad47689d017f82ac3e461b5a853605aafedc6879
blackboxtechgroup <86733864+kreynolds03@users.noreply.github.com>
2024-08-25 13:22:37 -0400

    minitorch project

--- src/minitorch/operators.py
+++ src/minitorch/operators.py
@@ -12,50 +12,54 @@ from typing import Callable, Iterable
 
 def mul(x: float, y: float) -> float:
     "$f(x, y) = x * y$"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return x*y
 
 
 def id(x: float) -> float:
     "$f(x) = x$"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return x
 
 
 def add(x: float, y: float) -> float:
     "$f(x, y) = x + y$"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return x + y
 
 
 def neg(x: float) -> float:
     "$f(x) = -x$"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return -x
 
 
 def lt(x: float, y: float) -> float:
     "$f(x) =$ 1.0 if x is less than y else 0.0"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    if x < y:
+        return 1.0
+    else:
+        return 0.0
 
 
 def eq(x: float, y: float) -> float:
     "$f(x) =$ 1.0 if x is equal to y else 0.0"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    if x == y:
+        return 1.0
+    else:
+        return 0.0
 
 
 def max(x: float, y: float) -> float:
     "$f(x) =$ x if x is greater than y else y"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    if x > y:
+        return x
+    else:
+        return y
 
 
 def is_close(x: float, y: float) -> float:
     "$f(x) = |x - y| < 1e-2$"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    if (abs(x-y)) < .01:
+        return 1.0
+    else:
+        return 0.0
 
 
 def sigmoid(x: float) -> float:
@@ -70,8 +74,12 @@ def sigmoid(x: float) -> float:
 
     for stability.
     """
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    if x >= 0:
+        y = 1.0/(1.0 + math.exp(-x))
+    else:
+        y = (math.exp(x)) / (1.0 + math.exp(x))
+    
+    return y
 
 
 def relu(x: float) -> float:
@@ -80,8 +88,10 @@ def relu(x: float) -> float:
 
     (See https://en.wikipedia.org/wiki/Rectifier_(neural_networks) .)
     """
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    if x > 0:
+        return x
+    else:
+        return 0
 
 
 EPS = 1e-6
@@ -105,20 +115,20 @@ def log_back(x: float, d: float) -> float:
 
 def inv(x: float) -> float:
     "$f(x) = 1/x$"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return 1.0 / x
 
 
 def inv_back(x: float, d: float) -> float:
     r"If $f(x) = 1/x$ compute $d \times f'(x)$"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return (-d/(x**2))
 
 
 def relu_back(x: float, d: float) -> float:
     r"If $f = relu$ compute $d \times f'(x)$"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    if x > 0:
+        return d
+    else:
+        return 0
 
 
 # ## Task 0.3
@@ -139,14 +149,20 @@ def map(fn: Callable[[float], float]) -> Callable[[Iterable[float]], Iterable[fl
          A function that takes a list, applies `fn` to each element, and returns a
          new list
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+    def apply_fn(values: Iterable[float]) -> Iterable[float]:
+        new_list = []
+        for value in range(values):
+            new_value = fn(value)
+            new_list.append(new_value)
+        return new_value
+    return apply_fn
 
 
 def negList(ls: Iterable[float]) -> Iterable[float]:
     "Use `map` and `neg` to negate each element in `ls`"
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+    negative_function = map(neg)
+    negative_values = negative_function(ls)
+    return negative_values
 
 
 def zipWith(
@@ -165,14 +181,22 @@ def zipWith(
          applying fn(x, y) on each pair of elements.
 
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+    def zip_fn(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
+        new_list = []
+        for item1, item2 in zip(ls1, ls2):
+            combined_val = fn(item1,item2)
+            new_list.append(combined_val)
+        return new_list
+    return zip_fn
+
 
 
 def addLists(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
     "Add the elements of `ls1` and `ls2` using `zipWith` and `add`"
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+    transform_fn = zipWith(add)
+    new_list = transform_fn(ls1, ls2)
+
+    return new_list
 
 
 def reduce(
@@ -190,17 +214,22 @@ def reduce(
          $x_1 \ldots x_n$ and computes the reduction :math:`fn(x_3, fn(x_2,
          fn(x_1, x_0)))`
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+    def reduce_fn(ls: Iterable[float]) -> float:
+        accumulator = start
+        for item in ls:
+            accumulator = fn(accumulator, item)
+        return accumulator
+    return reduce_fn
+
 
 
 def sum(ls: Iterable[float]) -> float:
     "Sum up a list using `reduce` and `add`."
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+    new_value = reduce(add,0)(ls)
+    return new_value
 
 
 def prod(ls: Iterable[float]) -> float:
     "Product of a list using `reduce` and `mul`."
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+    new_value = reduce(mul, 1)(ls)
+    return new_value
